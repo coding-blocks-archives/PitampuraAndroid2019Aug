a302ddfba9dac5a709437e6e1a137deecdb41e8c
Pulkit <aggarwalpulkit596@gmail.com>
2019-09-01 10:59:22 +0530

    add implcitIntents

--- Lecture04/Intents/app/src/main/java/com/codingblocks/intents/MainActivity.kt
+++ Lecture04/Intents/app/src/main/java/com/codingblocks/intents/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.codingblocks.intents
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,24 +9,43 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 const val NAME = "name"
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.button1 -> {
+                val i = Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:${editText.text}"))
+                i.putExtra(Intent.EXTRA_TEXT, "Hello")
+                i.putExtra(Intent.EXTRA_SUBJECT, "Sample Subject")
+                startActivity(i)
+            }
+            R.id.button2 -> {
+                val i = Intent(Intent.ACTION_VIEW, Uri.parse("http://${editText.text}"))
+                startActivity(Intent.createChooser(i, "Select Browser"))
+
+
+            }
+            R.id.button3 -> {
+                val i = Intent(Intent.ACTION_DIAL, Uri.parse("tel:${editText.text}"))
+                startActivity(i)
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button.setOnClickListener {
+        button1.setOnClickListener(this)
+        button2.setOnClickListener(this)
+        button3.setOnClickListener(this)
 
-            val a = editText.text.toString()
-
-            val i = Intent(this, Main2Activity::class.java)
-            i.putExtra(NAME, a)
-            startActivity(i)
-        }
     }
 
     fun implicit_intent(view: View) {
         val a = editText.text.toString()
 
+        val i = Intent(this, Main2Activity::class.java)
+        i.putExtra(NAME, a)
+        startActivity(i)
     }
 }
 
