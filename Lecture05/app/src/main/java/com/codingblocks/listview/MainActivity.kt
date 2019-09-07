1552dc0621361d333a90cdb6b61dde1fda76c889
Pulkit <aggarwalpulkit596@gmail.com>
2019-09-07 11:32:51 +0530

    Add Custom Adapter

--- Lecture05/app/src/main/java/com/codingblocks/listview/MainActivity.kt
+++ Lecture05/app/src/main/java/com/codingblocks/listview/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.codingblocks.listview
 
 import android.os.Bundle
-import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.item_movie.view.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,21 +22,23 @@ class MainActivity : AppCompatActivity() {
         R.drawable.blackpanther,
         R.drawable.avenger
     )
+    val list: ArrayList<Movie> = arrayListOf()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //
-//        lv = findViewById(R.id.lv)
-
-
-        list.addAll(
-            listOf(
-                Movie("Iron Man", "", "", 1)
+        for (i in 0..99) {
+            val random = Random().nextInt(5)
+            list.add(
+                Movie(
+                    movies[random],
+                    yearofRelease[random],
+                    actors[random],
+                    image[random]
+                )
             )
-        )
-        val movie = Movie("Iron Man", "", "", 1)
-        Log.i("Class", "" + movie.toString())
+        }
 
 //        lv.adapter =
 //            ArrayAdapter<String>(
@@ -38,7 +46,7 @@ class MainActivity : AppCompatActivity() {
 //                android.R.layout.simple_expandable_list_item_1,
 //                list
 //            )
-
+        lv.adapter = MovieAdapter(list)
 
     }
 }
@@ -49,3 +57,21 @@ data class Movie(
     val actor: String,
     val image: Int
 )
+
+class MovieAdapter(val movies: ArrayList<Movie>) : BaseAdapter() {
+    override fun getView(pos: Int, convertView: View?, parent: ViewGroup): View {
+        val inflator = LayoutInflater.from(parent.context)
+        val view = inflator.inflate(R.layout.item_movie, parent, false)
+        view.titleTv.text = movies[pos].name + "(" + movies[pos].year + ")"
+        view.actorTv.text = movies[pos].actor
+        view.imageView.setImageResource(movies[pos].image)
+        return view
+    }
+
+    override fun getItem(pos: Int): Movie = movies[pos]
+
+    override fun getItemId(pos: Int): Long = pos.toLong()
+
+
+    override fun getCount(): Int = movies.size
+}
