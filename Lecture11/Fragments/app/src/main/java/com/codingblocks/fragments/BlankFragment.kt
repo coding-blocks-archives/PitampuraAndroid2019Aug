62a81d30a2dd40b42524d3eda6e4520a3152acaf
Pulkit <aggarwalpulkit596@gmail.com>
2019-10-05 11:38:43 +0530

    use static fragments

--- Lecture11/Fragments/app/src/main/java/com/codingblocks/fragments/BlankFragment.kt
+++ Lecture11/Fragments/app/src/main/java/com/codingblocks/fragments/BlankFragment.kt
@@ -2,10 +2,12 @@ package com.codingblocks.fragments
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_blank.*
 
 /**
  * A simple [Fragment] subclass.
@@ -16,8 +18,20 @@ class BlankFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false)
+        val view = inflater.inflate(R.layout.fragment_blank, container, false)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val adapter = ArrayAdapter<String>(
+            requireContext(),
+            android.R.layout.simple_expandable_list_item_1,
+            arrayOf("Iron Man", "Black Panther", "", "", "", "")
+        )
+        lv.adapter = adapter
+
     }
 
 
