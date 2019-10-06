8db592c350121eb045e338b35292b857cf3c3571
Pulkit <aggarwalpulkit596@gmail.com>
2019-10-06 11:50:32 +0530

    viewpager with tabbed layout

--- Lecture12/ViewPagers/app/src/main/java/com/codingblocks/viewpagers/MainActivity.kt
+++ Lecture12/ViewPagers/app/src/main/java/com/codingblocks/viewpagers/MainActivity.kt
@@ -5,18 +5,31 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val adapter by lazy {
+       ViewPagerAdapter(supportFragmentManager)
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val adapter = ViewPagerAdapter(supportFragmentManager)
 
-        adapter.add(BlankFragment())
-        adapter.add(BlankFragment2())
-        adapter.add(BlankFragment3())
+        adapter.add(BlankFragment(),"!")
+        adapter.add(BlankFragment2(),"2")
+        adapter.add(BlankFragment3(),"3")
 
         pager.adapter = adapter
 
+        tabs.setupWithViewPager(pager)
+
+    }
+
+    override fun onBackPressed() {
+        if(pager.currentItem != 0){
+            pager.currentItem = 0
+        }else{
+            super.onBackPressed()
+        }
     }
 }
--- Lecture12/ViewPagers/app/src/main/java/com/codingblocks/viewpagers/ViewPagerAdapter.kt
+++ Lecture12/ViewPagers/app/src/main/java/com/codingblocks/viewpagers/ViewPagerAdapter.kt
@@ -7,13 +7,17 @@ import androidx.fragment.app.FragmentPagerAdapter
 class ViewPagerAdapter(fm:FragmentManager) : FragmentPagerAdapter(fm){
 
     private val fragments:ArrayList<Fragment> = ArrayList()
+    private val tabName:ArrayList<String> = ArrayList()
 
     override fun getItem(position: Int): Fragment = fragments[position]
 
     override fun getCount(): Int = fragments.size
 
-    fun add(fragment: Fragment){
+    fun add(fragment: Fragment,name:String){
         fragments.add(fragment)
+        tabName.add(name)
     }
+
+    override fun getPageTitle(position: Int): CharSequence? = tabName[position]
 
 }
