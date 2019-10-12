391ddd00d0c6b98f1bba625248aaae6e14a6d7a4
Pulkit <aggarwalpulkit596@gmail.com>
2019-10-12 12:30:47 +0530

    livedata example

--- Lecture13/NotesWithRoom/app/src/main/java/com/codingblocks/notewithroom/MainActivity.kt
+++ Lecture13/NotesWithRoom/app/src/main/java/com/codingblocks/notewithroom/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.codingblocks.notewithroom
 
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.room.Room
@@ -20,6 +23,7 @@ class MainActivity : AppCompatActivity() {
             .allowMainThreadQueries()
             .build()
     }
+    val toast = MutableLiveData<Boolean>()
 
 
     val todoList = arrayListOf<Todo>()
@@ -29,15 +33,31 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val adapter = TodoAdapter(todoList)
 
+        toast.observe(this, Observer {
+            button.isEnabled = it
+
+            if (it) {
+                Toast.makeText(this, "Show Toast", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Hide Toast", Toast.LENGTH_SHORT).show()
+            }
+        })
+
         db.todoDao().getAllTodo().observe(this, Observer {
             todoList.clear()
             todoList.addAll(it)
             adapter.notifyDataSetChanged()
+            Handler().postDelayed({
+                toast.value = true
+            },3000)
         })
         todoRv.layoutManager = LinearLayoutManager(this)
         todoRv.adapter = adapter
 
         button.setOnClickListener {
+            toast.value = false
+
+
             db.todoDao().insertTodo((Todo(name = editText.text.toString(), status = false)))
         }
     }
