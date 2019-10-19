9674e17e44c5f94f70ff1f36143beff7c50bf25f
Pulkit <aggarwalpulkit596@gmail.com>
2019-10-19 14:10:26 +0530

    File Read Write

---
+++ Lecture14-15/FileReadWrite/app/src/main/java/com/codingblocks/filereadwrite/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.codingblocks.filereadwrite
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Environment
+import android.util.Log
+import java.io.File
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.i("FILES","Files dir ${getExternalFilesDir(Environment.getDataDirectory().absolutePath)}")
+        /**
+        No need to ask permissions
+         **/
+//        val file = File(getExternalFilesDir(Environment.getDataDirectory().absolutePath),"myfile.txt")
+
+        val file = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),"myfile.txt")
+
+        file.writeText("Hello World")
+    }
+}
