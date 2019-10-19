75f6d9c9dd9b67318562a56d3738ab04e48ea675
Pulkit <aggarwalpulkit596@gmail.com>
2019-10-19 12:55:37 +0530

    Add Clickable Notifications

--- Lecture14-15/Notifications/app/src/main/java/com/codingblocks/notifications/MainActivity.kt
+++ Lecture14-15/Notifications/app/src/main/java/com/codingblocks/notifications/MainActivity.kt
@@ -2,11 +2,15 @@ package com.codingblocks.notifications
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,18 +30,44 @@ class MainActivity : AppCompatActivity() {
                 )
             )
         }
-
-        val simpleNotification = NotificationCompat.Builder(this, "first")
+        var simpleNotification = NotificationCompat.Builder(this, "first")
             .setContentTitle("Simple Notification")
             .setContentText("Sample Description for the notification")
-            .setStyle(
-                NotificationCompat.BigTextStyle()
-                    .bigText("Hello world this is the bigger txt,Hello world this is the bigger txt,Hello world this is the bigger txt,Hello world this is the bigger txt,Hello world this is the bigger txt,Hello world this is the bigger txt")
-            )
             .setSmallIcon(R.drawable.ic_launcher_foreground)
-            .build()
 
-        nm.notify(System.currentTimeMillis().toInt(), simpleNotification)
+        button.setOnClickListener {
+            val notification = simpleNotification.build()
+            nm.notify(System.currentTimeMillis().toInt(), notification)
+        }
+
+        button2.setOnClickListener {
+            val i = Intent()
+            i.action = Intent.ACTION_VIEW
+            i.data = Uri.parse("https://www.google.com")
+
+            val pi = PendingIntent.getActivity(this, 123, i, PendingIntent.FLAG_UPDATE_CURRENT)
+            val notification = simpleNotification.apply {
+                setContentIntent(pi)
+                setAutoCancel(true)
+            }.build()
+
+            nm.notify(System.currentTimeMillis().toInt(), notification)
+
+        }
+
+        button3.setOnClickListener {
+            val i = Intent()
+            i.action = Intent.ACTION_VIEW
+            i.data = Uri.parse("https://www.google.com")
+
+            val pi = PendingIntent.getActivity(this, 123, i, PendingIntent.FLAG_UPDATE_CURRENT)
+            val notification = simpleNotification.apply {
+                addAction(R.drawable.ic_launcher_foreground,"Click Me",pi)
+            }.build()
+
+            nm.notify(System.currentTimeMillis().toInt(), notification)
+
+        }
 
 
     }
