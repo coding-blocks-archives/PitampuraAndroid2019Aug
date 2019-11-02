c3af0969e75d564a217b98195da23a7e15425cb2
Pulkit <aggarwalpulkit596@gmail.com>
2019-11-02 11:08:10 +0530

    Send Data

--- Leture17/app/src/main/java/com/android/firebasedatabase/MainActivity.kt
+++ Leture17/app/src/main/java/com/android/firebasedatabase/MainActivity.kt
@@ -2,15 +2,12 @@ package com.android.firebasedatabase
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.database.ChildEventListener
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    val db by lazy{
+    val db by lazy {
         FirebaseDatabase.getInstance()
     }
 
@@ -18,10 +15,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        button.setOnClickListener {
+            val ref = db.reference.child("username")
+            val key = ref.push().key
+            key?.let { it1 -> ref.child(it1).setValue(editText.text.toString()) }
+        }
+
 
         val list = arrayListOf<String>()
+        db.reference.child("username").child("1").addValueEventListener(
+            object : ValueEventListener {
+                override fun onDataChange(p0: DataSnapshot) {
+                    textView.setText(textView.text.toString() + p0.value.toString())
+                }
+
+                override fun onCancelled(p0: DatabaseError) {
+                }
+
+            }
+        )
+
         db.reference.child("username")
-            .addChildEventListener(object : ChildEventListener{
+            .addChildEventListener(object : ChildEventListener {
                 override fun onCancelled(p0: DatabaseError) {
                 }
 
@@ -29,10 +44,10 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 override fun onChildChanged(p0: DataSnapshot, p1: String?) {
-                    textView.text = p0.value.toString()
                 }
 
                 override fun onChildAdded(p0: DataSnapshot, p1: String?) {
+
                     list.add(p0.value.toString())
                     textView.text = ""
                     list.forEach {
